24fb294f99b47d9cdff28b5e8e5c6fb429584d40
CodeSandbox <support@codesandbox.io>
2023-12-16 16:56:28 +0000

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,52 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <p>Day: {{ getDay() }}</p>
+    <p>Month: {{ getMonth() }}</p>
+    <button @click="addDay">Add Day</button>
+    <button @click="addMonth(1)">Add Month</button>
+    <button @click="resetDate">Reset</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+export default {
+  data() {
+    return {
+      initialDate: new Date(),
+      date: new Date(),
+    };
+  },
+  methods: {
+    getDay() {
+      return this.date.getDate();
+    },
+    getMonth() {
+      return this.date.getMonth() + 1;
+    },
+    addDay() {
+      const newDate = new Date(this.date);
+      newDate.setDate(newDate.getDate() + 1);
+
+      if (newDate.getMonth() !== this.date.getMonth()) {
+        newDate.setDate(1);
+      }
+      this.date = newDate;
+    },
+    addMonth(numberOfMonths) {
+      const newDate = new Date(this.date);
+      newDate.setMonth(newDate.getMonth() + numberOfMonths);
+
+      if (newDate.getMonth() !== (this.date.getMonth() + numberOfMonths) % 12) {
+        newDate.setDate(0);
+        newDate.setMonth(0);
+      }
+      this.date = newDate;
+    },
+    resetDate() {
+      // Set both day and month to zero
+      this.date.setDate(0);
+      this.date.setMonth(0);
+    },
+  },
+};
+</script>
